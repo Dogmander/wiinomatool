d79c0f87b68743ee165a67ebc367acce4de9b8ae
6100m <31662463+6100m@users.noreply.github.com>
2020-10-07 14:46:46 -0600

    Add rest of tools 1/2

---
+++ DebugTools/src/extract.bat
@@ -0,0 +1,7 @@
+echo You really should not be using this version... press enter if you still want to use the bat version.
+pause
+call cmd.exe /k ../7z.exe e src.zip
+echo Done. Press Enter to exit.
+pause
+cls
+exit
